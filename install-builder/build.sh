1c7b86b1cefc0e48b058aba58f17e0d2ff0b7495
Zapta <zapta@users.noreply.github.com>
2025-02-14 19:20:42 -0800

    Updated darwin-64 installer

--- install-builder/build.sh
+++ install-builder/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-rm -rf output/apio-*-osx-installer.app
-rm -rf output/apio-*-osx-installer.dmg
+VER="0.9.6"
+PLATFORM="darwin-arm64"
+
+rm -rf output/apio-*${VER}-*.app
+rm -rf output/apio-*${VER}-*.dmg
 
 builder="/Applications/InstallBuilder Professional 24.11.1/bin/builder"
 
-"$builder" build apio-project.xml osx
+"$builder" build apio-project.xml osx \
+   --setvars PLATFORM="${PLATFORM}" VER="${VER}" 
 
-rm -rf output/apio-*-osx-installer.app
+rm -rf output/apio-*.app
 
-ls -al output
+# apio-0.9.6-osx-installer.dmg
+mv output/apio-${VER}-*.dmg output/apio-${PLATFORM}-${VER}-installer.dmg
+
+ls -l output
