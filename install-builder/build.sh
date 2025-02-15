6254fc080755a65f5a1913b885dcf3981e84b4e2
Zapta <zapta@users.noreply.github.com>
2025-02-14 22:14:06 -0800

    Build installers for all three platforms.

--- install-builder/build.sh
+++ install-builder/build.sh
@@ -1,20 +1,38 @@
-#!/bin/bash
+#!/bin/bash -x
 
 VER="0.9.6"
-PLATFORM="darwin-arm64"
 
-rm -rf output/apio-*${VER}-*.app
-rm -rf output/apio-*${VER}-*.dmg
+# Exit on any error.
+set -e
 
 builder="/Applications/InstallBuilder Professional 24.11.1/bin/builder"
 
-"$builder" build apio-project.xml osx \
-   --verbose \
-   --setvars project.version=${VER} VER=${VER} PLATFORM=${PLATFORM}
+function build_platform() {
+  platform_id=$1
+  os=$2
 
-rm -rf output/apio-*.app
+  echo
+  echo "***** Building ${platform_id} / ${VER} (${os})"
+  echo
+  sleep 1.0
 
-# apio-0.9.6-osx-installer.dmg
-mv output/apio-${VER}-*.dmg output/apio-${PLATFORM}-${VER}-installer.dmg
+  "$builder" build apio-project.xml ${os} \
+      --verbose \
+      --setvars project.version=${VER} VER=${VER} PLATFORM=${platform_id}
+}
 
+
+rm -rf output/*
+
+build_platform "darwin-arm64" "osx"
+build_platform "windows-amd64" "windows-x64"
+build_platform "linux-x86-64" "linux-x64"
+
+
+rm -rf output/apio-*-osx-installer.app
+mv output/apio-${VER}-osx-installer.dmg output/apio-darwin-arm64-${VER}-installer.dmg
+mv output/apio-${VER}-windows-x64-installer.exe output/apio-windows-amd64-${VER}-installer.exe
+mv output/apio-${VER}-linux-x64-installer.run output/apio-linux-x86-64-${VER}-installer.run
+
+echo
 ls -l output
