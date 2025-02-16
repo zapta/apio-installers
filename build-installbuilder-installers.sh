5071b20baa9aa6be00528ebba4a45c4c67949e7f
Zapta <zapta@users.noreply.github.com>
2025-02-16 12:14:53 -0800

    Tweaked scripts.

---
+++ build-installbuilder-installers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Build InstallBuilder installers for the pyinstaller packages.
+#
+# Requirements:
+#  1. InstallBuilder installed.
+
+cd install-builder
+./build-installers.sh
+
---
+++ build-pyinstaller-package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Builds a pyinstaller package for the current platform.
+#
+# Requirements:
+#   1. Apio installed using 'pip install -e .'
+#   2. Pyinstaller installed using 'pip install pyinstaller'. 
+
+cd pyinstaller
+
+python3 build-package.py
+
