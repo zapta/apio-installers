3dd3d8c448210a77049360cd22a3bbf9250ab913
Zapta <zapta@users.noreply.github.com>
2025-02-17 20:04:51 -0800

    Osx installer now determines dynamically the apio path.

--- installers/build-osx.sh
+++ installers/build-osx.sh
@@ -3,21 +3,19 @@
 # Exit on any error
 set -e
 
-for d in _package; do
-  rm -rf ./$d
-  mkdir ./$d
-done
+rm -rf _package _dist	_work
+mkdir -p _package/apio
 
-mkdir _package/apio
 pushd _package/apio
 unzip ../../../pyinstaller/packages/pyinstaller-apio-darwin-arm64-0.9.6-package.zip
 popd
 
+# See 'man pkgbuild' for details.
 pkgbuild \
   --root "_package"\
   --install-location "/Applications" \
-  --identifier "com.example.myapp" \
+  --identifier "io.github.fpgawars.apio" \
   --version "0.9.6" \
-  --scripts scripts \
+  --scripts Scripts \
   --ownership recommended \
   "packages/installer-apio-darwin-arm64-0.9.6.pkg"
